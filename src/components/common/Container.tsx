e9a777003e3a0213ee6c6ecd199147d580acd09e
Rahul Mondal <rahulmondar@gmail.com>
2023-11-05 16:10:52 +0530

    feat: added container

---
+++ src/components/common/Container.tsx
@@ -0,0 +1,19 @@
+import cn from "@/lib/cn";
+import { ComponentPropsWithoutRef } from "react";
+
+const Container = ({
+    children,
+    className,
+    ...props
+}: ComponentPropsWithoutRef<"div">) => {
+    return (
+        <div
+            className={cn("max-w-7xl px-4 md:px-6 mx-auto relative", className)}
+            {...props}
+        >
+            {children}
+        </div>
+    );
+};
+
+export default Container;
--- src/components/sub/HeroContent.tsx
+++ src/components/sub/HeroContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Container from "@/components/common/Container";
 import {
     slideInFromLeft,
     slideInFromRight,
@@ -14,55 +15,57 @@ const HeroContent = () => {
         <motion.div
             initial="hidden"
             animate="visible"
-            className="flex flex-col lg:flex-row gap-6 items-center justify-center px-20 mt-40 w-full z-[20]"
+            className="mt-40 w-full z-[20]"
         >
-            <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
-                <motion.div
-                    variants={slideInFromTop(0.5)}
-                    className="welcome-box py-2 px-3 border border-[#7042f88b] opacity-90 rounded-full flex items-center gap-2"
-                >
-                    <SparklesIcon className="text-purple-400 h-5 w-5" />
-                    <h1 className="welcome-text text-[13px]">
-                        Full-Stack Developer
-                    </h1>
+            <Container className="flex flex-col lg:flex-row gap-6 items-center justify-center">
+                <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+                    <motion.div
+                        variants={slideInFromTop(0.5)}
+                        className="welcome-box py-2 px-3 border border-[#7042f88b] opacity-90 rounded-full flex items-center gap-2"
+                    >
+                        <SparklesIcon className="text-purple-400 h-5 w-5" />
+                        <h1 className="welcome-text text-[13px]">
+                            Full-Stack Developer
+                        </h1>
+                    </motion.div>
+                    <motion.div
+                        variants={slideInFromLeft(0.5)}
+                        className="whitespace-nowrap flex-wrap flex gap-2 font-semibold text-5xl leading-[3.5rem] text-bold text-white max-w-[37.5rem] w-auto mt-5"
+                    >
+                        Carrying
+                        <span className="whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+                            your ideas
+                        </span>
+                        <span>to</span>
+                        their
+                        <span className="whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-500">
+                            destination
+                        </span>
+                    </motion.div>
+                    <motion.p
+                        variants={slideInFromLeft(0.8)}
+                        className="max-w-[37.5rem] text-gray-400"
+                    >
+                        I&apos;m a Full-Stack Developer. Flexible, hard worker,
+                        ready to learn and contribute to team success. Working
+                        confidently with a wide range of technologies.
+                    </motion.p>
+                    <motion.a
+                        variants={slideInFromLeft(1)}
+                        className="button-primary rounded-lg mt-6 px-7 py-3 w-max cursor-pointer"
+                    >
+                        Download CV
+                    </motion.a>
+                </div>
+                <motion.div variants={slideInFromRight(0.8)} className="">
+                    <Image
+                        src="/image/mainIconsDark.svg"
+                        alt="Hero Icons"
+                        height={650}
+                        width={650}
+                    />
                 </motion.div>
-                <motion.div
-                    variants={slideInFromLeft(0.5)}
-                    className="whitespace-nowrap flex-wrap flex gap-2 font-semibold text-5xl leading-[3.5rem] text-bold text-white max-w-[37.5rem] w-auto mt-5"
-                >
-                    Carrying
-                    <span className="whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-                        your ideas
-                    </span>
-                    <span>to</span>
-                    their
-                    <span className="whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-500">
-                        destination
-                    </span>
-                </motion.div>
-                <motion.p
-                    variants={slideInFromLeft(0.8)}
-                    className="max-w-[37.5rem] text-gray-400"
-                >
-                    I&apos;m a Full-Stack Developer. Flexible, hard worker,
-                    ready to learn and contribute to team success. Working
-                    confidently with a wide range of technologies.
-                </motion.p>
-                <motion.a
-                    variants={slideInFromLeft(1)}
-                    className="button-primary rounded-lg mt-6 px-7 py-3 w-max cursor-pointer"
-                >
-                    Download CV
-                </motion.a>
-            </div>
-            <motion.div variants={slideInFromRight(0.8)} className="">
-                <Image
-                    src="/image/mainIconsDark.svg"
-                    alt="Hero Icons"
-                    height={650}
-                    width={650}
-                />
-            </motion.div>
+            </Container>
         </motion.div>
     );
 };
