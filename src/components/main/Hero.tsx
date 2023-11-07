239f9ff472ec17513c3e086e040cfa2fcda74379
Rahul Mondal <rahulmondar@gmail.com>
2023-11-07 10:50:33 +0530

    fix: minor change in bg-video position

--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -3,15 +3,17 @@ import HeroContent from "@/components/sub/HeroContent";
 const Hero = () => {
     return (
         <div className="relative flex flex-col h-full w-full">
-            <div className="absolute h-screen w-full">
-                <video
-                    autoPlay
-                    loop
-                    muted
-                    className="absolute rotate-180 -top-[40%] left-0 w-full h-full object-cover"
-                >
-                    <source src="/video/blackhole.webm" type="video/webm" />
-                </video>
+            <div className="absolute top-0 h-screen w-full overflow-hidden">
+                <div className="absolute h-[calc(100vh+15rem)] w-full">
+                    <video
+                        autoPlay
+                        loop
+                        muted
+                        className="absolute z-[-1] rotate-180 -top-[42%] left-0 w-full h-full object-content"
+                    >
+                        <source src="/video/blackhole.webm" type="video/webm" />
+                    </video>
+                </div>
             </div>
             <HeroContent />
         </div>
--- src/components/sub/Navbar.tsx
+++ src/components/sub/Navbar.tsx
@@ -5,12 +5,12 @@ import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <nav className="flex items-center gap-2 py-1 px-2 rounded-full border border-purple-400/40 bg-purple-900/40">
+        <nav className="flex items-center gap-2 py-1 px-2 rounded-full border border-purple-400/50 bg-purple-900/10">
             {NavItems.map(({ path, text }) => (
                 <Link
                     href={path}
                     key={text}
-                    className="py-[0.4rem] px-5 rounded-full font-medium text-gray-300"
+                    className="py-[0.4rem] px-5 rounded-full font-semibold text-gray-300"
                 >
                     {text}
                 </Link>
