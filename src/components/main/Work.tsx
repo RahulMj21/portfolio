cfe2f6a52b01856cc8e03fd4b934ecd533220f59
Rahul Mondal <rahulmondar@gmail.com>
2023-11-22 10:51:08 +0530

    feat: added cards in work section

--- src/components/main/Work.tsx
+++ src/components/main/Work.tsx
@@ -1,8 +1,35 @@
+"use client";
+
+import WorkCard from "@/components/sub/WorkCard";
+import { slideInFromTop } from "@/utils/motion";
+import { motion } from "framer-motion";
+
 const Work = () => {
     return (
-        <section id="work" className="min-h-screen py-20">
-            Work
-        </section>
+        <motion.section
+            id="work"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            className="py-20 flex flex-col gap-14"
+        >
+            <motion.h1
+                variants={slideInFromTop(0.5)}
+                className="text-[2.2rem] sm:text-[3rem] md:text-[3.8rem] font-semibold text-center"
+            >
+                <span className="mr-4 bg-gradient-to-br bg-clip-text text-transparent from-purple-600 to-cyan-700">
+                    My
+                </span>
+                Work
+            </motion.h1>
+            <div className="flex flex-wrap justify-center gap-y-20 gap-x-10">
+                <WorkCard />
+                <WorkCard />
+                <WorkCard />
+                <WorkCard />
+                <WorkCard />
+            </div>
+        </motion.section>
     );
 };
 
